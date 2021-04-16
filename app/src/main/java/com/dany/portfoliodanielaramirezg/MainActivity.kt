5b5cb01b62e83ac2e995f376d6e054f3aec49967
danyramirezg <danyramirezg@gmail.com>
2021-04-16 17:12:07 -0500

    Layouts and setup project

--- app/src/main/java/com/dany/portfoliodanielaramirezg/MainActivity.kt
+++ app/src/main/java/com/dany/portfoliodanielaramirezg/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.dany.portfoliodanielaramirezg
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+//import android.support.design.widget.TabLayout;
+import com.dany.portfoliodanielaramirezg.controller.DevPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the view Pager
+        viewPager.adapter = DevPagerAdapter(supportFragmentManager)
+
+        // Setup the tab-Layout
+
+        tableLayoutId.setupWithViewPager(viewPager)
+//
+//        tabLayout = findViewById<TabLayout>(R.id.tableLayoutId);
+//        tabLayout.setupWithViewPager(viewpager)
+//        tableLayoutId.setUpWithViewPager(viewPager)
+//
+//        tableLayoutId.setTabTextColors(color.GRAY, color.paseColor("white"))
     }
 }
--- app/src/main/java/com/dany/portfoliodanielaramirezg/controller/DevPagerAdapter.kt
+++ app/src/main/java/com/dany/portfoliodanielaramirezg/controller/DevPagerAdapter.kt
@@ -1,4 +1,34 @@
 package com.dany.portfoliodanielaramirezg.controller
 
-class DevPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.dany.portfoliodanielaramirezg.view.AboutFragment
+import com.dany.portfoliodanielaramirezg.view.ContactFragment
+import com.dany.portfoliodanielaramirezg.view.SkillsFragment
+import com.dany.portfoliodanielaramirezg.view.WorkFragment
+
+class DevPagerAdapter(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0 -> return AboutFragment()
+            1 -> return WorkFragment()
+            2 -> return SkillsFragment()
+            else -> return ContactFragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        return 4
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position){
+            0 -> return "ABOUT"
+            1 -> return "WORK"
+            2 -> return "SKILLS"
+            3 -> return "CONTACT"
+        }
+        return null
+    }
 }
